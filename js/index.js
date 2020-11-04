7da5b0a933578abc09b6ab8c73d0d35bb1fc47a0
marcodev05 <73964346+marcodev05@users.noreply.github.com>
2020-11-04 22:54:55 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$("h2").click(function(){
+    $("h2").text("tongasoa eto amin'ity site ity tompoko");
+})
+
+$("button").click(function(){
+    $(".container2").slideToggle();
+    $(".container2").css("display", "block");
+    
+})
